7e21800cf133c5a87e7b5d594034ee796e5fd949
compcoon <klimbasov.al@gmail.com>
2022-11-17 14:56:22 +0300

    after long time of self destruction...

--- src/app/_service/user/user.service.ts
+++ src/app/_service/user/user.service.ts
@@ -1,27 +1,32 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { observable, Observable } from 'rxjs';
+import { SimpleUser } from 'src/app/_models/simple-user';
 import { environment } from 'src/environments/environment';
-import { User } from '../../_models/user/user';
+import { User } from '../../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private baseUrl = environment.baseUrl + 'admin/users/';
+  private baseUrl = environment.baseUrl;
   constructor(private http: HttpClient) { }
 
-    public getAll(page: number): Observable<User[]> {
-        return this.http.get<User[]>(`${this.baseUrl}?page=${page}`);
-    }
-    public getById(userId: number): Observable<User> {
-        return this.http.get<User>(`${this.baseUrl}${userId}`);
-    }
-    public delete(userId: number) {
-      return this.http.delete<User>(`${this.baseUrl}${userId}`);
-    }
-    public create(user: User) {
-      return this.http.post<User>(`${this.baseUrl}`, user);
-    }
+  public getByName(username: string, page: number): Observable<SimpleUser[]> {
+    return this.http.get<SimpleUser[]>(`${this.baseUrl}users/?partialName=${username}&page=${page}`);
+
+  }
+  public delete(userId: number) {
+    return this.http.delete<User>(`${this.baseUrl}admin/users/${userId}`);
+  }
+  public count(): Observable<number> {
+    //return this.http.get<number>(`${this.baseUrl}count`);
+    return new Observable<number>((observer) => {
+      observer.next(100);
+    });
+  }
+  public update(user: User, id: number): Observable<User> {
+    return this.http.patch<User>(`${this.baseUrl}admin/users/${id}`, user);
+  }
 }
